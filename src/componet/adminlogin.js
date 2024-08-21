a92256e7fd4c3ddadfacf030b696213f1c814def
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-21 11:27:12 +0530

    add loging page bg img

--- src/componet/adminlogin.js
+++ src/componet/adminlogin.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../cssfile/Admin.css";
-import { Link } from "react-router-dom";
 
 const Adminlogin = () => {
   const initialdata = {
@@ -50,8 +49,69 @@ const Adminlogin = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="main bg-white rounded-lg shadow-md p-10 w-96">
+    // <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
+    //   <div className="main bg-white rounded-lg shadow-md p-10 w-96">
+    //     {showerror && (
+    //       <div className="text-red-500 font-bold text-center mb-3">
+    //         Invalid email or password
+    //       </div>
+    //     )}
+    //     <h1 className="text-green-500 text-3xl mb-6">Admin Login</h1>
+
+    //     <form onSubmit={handleSubmit}>
+    //       <label
+    //         htmlFor="first"
+    //         className="text-left text-gray-700 font-bold block mb-2"
+    //       >
+    //         Email
+    //       </label>
+    //       <input
+    //         type="text"
+    //         name="email"
+    //         onChange={handleChange}
+    //         value={inputlogindata.email}
+    //         id="email"
+    //         placeholder="Enter your Email"
+    //         className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500"
+    //         required
+    //       />
+
+    //       <label
+    //         htmlFor="password"
+    //         className="text-left text-gray-700 font-bold block mt-4 mb-2"
+    //       >
+    //         Password
+    //       </label>
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         id="password"
+    //         onChange={handleChange}
+    //         value={inputlogindata.password}
+    //         placeholder="Enter your Password"
+    //         className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500 mt-1"
+    //         required
+    //       />
+
+    //       <div className="flex justify-center w-100 mt-6">
+    //         <button
+    //           type="submit"
+    //           className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition w-100 duration-300"
+    //         >
+    //           Submit
+    //         </button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
+    <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50"
+        style={{
+          backgroundImage: `url(${require("./../img/bus.jpg")})`,
+        }}
+      ></div>
+      <div className="relative main bg-gray-300 rounded-lg shadow-md p-10 w-96 z-10">
         {showerror && (
           <div className="text-red-500 font-bold text-center mb-3">
             Invalid email or password
@@ -61,7 +121,7 @@ const Adminlogin = () => {
 
         <form onSubmit={handleSubmit}>
           <label
-            htmlFor="first"
+            htmlFor="email"
             className="text-left text-gray-700 font-bold block mb-2"
           >
             Email
