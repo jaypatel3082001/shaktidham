d36313a2d17463cc183e45a536bf57e7e33fb4ea
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-21 14:15:42 +0530

    add loginpage with figma

--- src/componet/adminlogin.js
+++ src/componet/adminlogin.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../cssfile/Admin.css";
+import Bus from "./../img/Adminimg.jpg";
+import { ReactComponent as Vector } from "../svg/Vector.svg";
 
 const Adminlogin = () => {
   const initialdata = {
@@ -104,65 +106,132 @@ const Adminlogin = () => {
     //     </form>
     //   </div>
     // </div>
-    <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
-      <div
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50"
-        style={{
-          backgroundImage: `url(${require("./../img/bus.jpg")})`,
-        }}
-      ></div>
-      <div className="relative main bg-gray-300 rounded-lg shadow-md p-10 w-96 z-10 mt-32">
-        {showerror && (
-          <div className="text-red-500 font-bold text-center mb-3">
-            Invalid email or password
-          </div>
-        )}
-        <h1 className="text-green-500 text-3xl mb-6">Admin Login</h1>
+    // <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
+    //   <div
+    //     className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50"
+    //     style={{
+    //       backgroundImage: `url(${require("./../img/bus.jpg")})`,
+    //     }}
+    //   ></div>
+    //   <div className="relative main bg-gray-300 rounded-lg shadow-md p-10 w-96 z-10 mt-32">
+    //     {showerror && (
+    //       <div className="text-red-500 font-bold text-center mb-3">
+    //         Invalid email or password
+    //       </div>
+    //     )}
+    //     <h1 className="text-green-500 text-3xl mb-6">Admin Login</h1>
 
-        <form onSubmit={handleSubmit}>
-          <label
-            htmlFor="email"
-            className="text-left text-gray-700 font-bold block mb-2"
-          >
-            UserName
-          </label>
-          <input
-            type="text"
-            name="email"
-            onChange={handleChange}
-            value={inputlogindata.email}
-            id="email"
-            placeholder="Enter your Email"
-            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500"
-            required
-          />
+    //     <form onSubmit={handleSubmit}>
+    //       <label
+    //         htmlFor="email"
+    //         className="text-left text-gray-700 font-bold block mb-2"
+    //       >
+    //         UserName
+    //       </label>
+    //       <input
+    //         type="text"
+    //         name="email"
+    //         onChange={handleChange}
+    //         value={inputlogindata.email}
+    //         id="email"
+    //         placeholder="Enter your Email"
+    //         className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500"
+    //         required
+    //       />
 
-          <label
-            htmlFor="password"
-            className="text-left text-gray-700 font-bold block mt-4 mb-2"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={handleChange}
-            value={inputlogindata.password}
-            placeholder="Enter your Password"
-            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500 mt-1"
-            required
-          />
+    //       <label
+    //         htmlFor="password"
+    //         className="text-left text-gray-700 font-bold block mt-4 mb-2"
+    //       >
+    //         Password
+    //       </label>
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         id="password"
+    //         onChange={handleChange}
+    //         value={inputlogindata.password}
+    //         placeholder="Enter your Password"
+    //         className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500 mt-1"
+    //         required
+    //       />
 
-          <div className="flex justify-center w-100 mt-6">
-            <button
-              type="submit"
-              className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition w-100 duration-300"
+    //       <div className="flex justify-center w-100 mt-6">
+    //         <button
+    //           type="submit"
+    //           className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition w-100 duration-300"
+    //         >
+    //           Submit
+    //         </button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
+    <div className="flex flex-col lg:flex-row items-center justify-between p-4">
+      <div className="w-full lg:w-1/3 mb-6 lg:mb-0">
+        <img src={Bus} alt="" className="w-full h-auto" />
+      </div>
+
+      <div className="flex items-center justify-center w-full lg:w-1/3">
+        <div className="main rounded-lg p-6 sm:p-8 md:p-10 w-full sm:w-80 md:w-96">
+          {showerror && (
+            <div className="text-red-500 font-bold text-center mb-3">
+              Invalid email or password
+            </div>
+          )}
+          <h1 className="text-black text-2xl sm:text-3xl mb-4 sm:mb-6">
+            Login Details
+          </h1>
+
+          <form onSubmit={handleSubmit}>
+            <label
+              htmlFor="email"
+              className="text-left text-gray-700 font-bold block"
             >
-              Submit
-            </button>
-          </div>
-        </form>
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              onChange={handleChange}
+              value={inputlogindata.email}
+              id="email"
+              placeholder="Enter your Email"
+              className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500"
+              required
+            />
+
+            <label
+              htmlFor="password"
+              className="text-left text-gray-700 font-bold block mt-4 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange}
+              value={inputlogindata.password}
+              placeholder="Enter your Password"
+              className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-green-500 mt-1"
+              required
+            />
+
+            <div className="flex justify-center mt-6">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-300 w-full"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="w-full lg:w-1/3 mt-6 lg:mt-0">
+        <Vector />
       </div>
     </div>
   );
