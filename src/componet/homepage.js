87666c85347252a7db244977465abbfbf53e99ca
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-21 14:50:50 +0530

    add

--- src/componet/homepage.js
+++ src/componet/homepage.js
@@ -599,13 +599,15 @@ const Homepage = () => {
     //     setData={setData}
     //   />
     // </div>
-    <div className="App p-4 md:p-6 lg:p-8 flex flex-col gap-4 md:flex-row">
+    <div>
       <div className="flex flex-col lg:flex-row items-center justify-between">
-        <div className="w-full lg:w-1/3 mb-6 lg:mb-0">
-          <img src={Bus} alt="" className="w-full h-auto" />
-        </div>
+        {!isDateSelected && (
+          <div className="w-full mb-6 lg:mb-0 lg:w-1/3 flex justify-center lg:justify-start">
+            <img src={Bus} alt="" className="w-full h-auto object-cover" />
+          </div>
+        )}
 
-        <div className="flex justify-center w-full lg:w-1/3 mb-6 lg:mb-0">
+        <div className="w-full mb-6 lg:mb-0 lg:w-1/3 flex justify-center pt-2">
           <LocalizationProvider
             dateAdapter={AdapterLuxon}
             adapterLocale="en-gb"
@@ -634,122 +636,128 @@ const Homepage = () => {
           </LocalizationProvider>
         </div>
 
-        <div className="w-full lg:w-1/3 lg:mt-0">
-          <Vector />
-        </div>
+        {!isDateSelected && (
+          <div className="w-full lg:w-1/3 lg:mt-0 flex justify-center lg:justify-end">
+            <Vector />
+          </div>
+        )}
       </div>
+      <div className="App p-2 md:p-6 lg:p-8 flex flex-col gap-4 md:flex-row">
+        {!isDateSelected && !isloading ? (
+          <div className="text-center"></div>
+        ) : isloading ? (
+          <div className="text-center py-4">Loading...</div>
+        ) : (
+          <div>
+            <div className="flex justify-between mb-3">
+              <button
+                className="bg-[#8A6FDF] text-white px-4 py-2 rounded hover:bg-[#7451f2] mt-2"
+                onClick={handleDownload}
+              >
+                Download
+              </button>
+              <button
+                className="bg-[#8A6FDF] text-white px-4 py-2 rounded hover:bg-[#7451f2] mt-2"
+                onClick={showQuestion}
+              >
+                Add
+              </button>
+            </div>
 
-      {!isDateSelected && !isloading ? (
-        <div className="text-center"></div>
-      ) : isloading ? (
-        <div className="text-center py-4">Loading...</div>
-      ) : (
-        <div>
-          <div className="flex justify-between mb-3">
-            <button
-              className="bg-[#8A6FDF] text-white px-4 py-2 rounded hover:bg-[#7451f2] mt-2"
-              onClick={handleDownload}
-            >
-              Download
-            </button>
-            <button
-              className="bg-[#8A6FDF] text-white px-4 py-2 rounded hover:bg-[#7451f2] mt-2"
-              onClick={showQuestion}
-            >
-              Add
-            </button>
-          </div>
-
-          <div className="flex-1 overflow-x-auto" id="table-container">
-            <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
-              <thead className="bg-gray-100">
-                <tr>
-                  <th className="p-2 border-b">Number</th>
-                  <th className="p-2 border-b">Village</th>
-                  <th className="p-2 border-b">Name</th>
-                  <th className="p-2 border-b">Phone No.</th>
-                  <th className="p-2 border-b">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {[...Array(30).keys()].map((i) => {
-                  const currentLabel = getLabel(i).toString();
-                  const item = sortdata.data?.find(
-                    (item) => item.seatNumber === currentLabel
-                  );
-                  return (
-                    <tr key={i} className="hover:bg-gray-50">
-                      <td className="text-center py-5 border">
-                        {currentLabel}
-                      </td>
-                      <td className="p-2 border">{item ? item.vilage : ""}</td>
-                      <td className="p-2 border">{item ? item.name : ""}</td>
-                      <td className="p-2 border">{item ? item.mobile : ""}</td>
-                      <td className="relative border">
-                        <>
-                          <button
-                            className="ml-4 hover:text-blue-900"
-                            onClick={() => handleClicktd(currentLabel)}
-                            ref={(el) => (buttonRefs.current[i] = el)}
-                          >
-                            <div className="flex justify-center">
-                              <Action className="w-6 h-6 text-blue-500" />
-                            </div>
-                          </button>
-                          {display && tooltipId === currentLabel && (
-                            <div
-                              role="tooltip"
-                              className="absolute shadow-lg bg-blue-400 z-10 border rounded p-2"
-                              style={{
-                                top: "100%",
-                                left: "50%",
-                                transform: "translateX(-50%)",
-                              }}
-                              ref={tooltipRef}
+            <div className="flex-1 overflow-x-auto" id="table-container">
+              <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
+                <thead className="bg-gray-100">
+                  <tr>
+                    <th className="p-2 border-b">Number</th>
+                    <th className="p-2 border-b">Village</th>
+                    <th className="p-2 border-b">Name</th>
+                    <th className="p-2 border-b">Phone No.</th>
+                    <th className="p-2 border-b">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {[...Array(30).keys()].map((i) => {
+                    const currentLabel = getLabel(i).toString();
+                    const item = sortdata.data?.find(
+                      (item) => item.seatNumber === currentLabel
+                    );
+                    return (
+                      <tr key={i} className="hover:bg-gray-50">
+                        <td className="text-center py-5 border">
+                          {currentLabel}
+                        </td>
+                        <td className="p-2 border">
+                          {item ? item.vilage : ""}
+                        </td>
+                        <td className="p-2 border">{item ? item.name : ""}</td>
+                        <td className="p-2 border">
+                          {item ? item.mobile : ""}
+                        </td>
+                        <td className="relative border">
+                          <>
+                            <button
+                              className="ml-4 hover:text-blue-900"
+                              onClick={() => handleClicktd(currentLabel)}
+                              ref={(el) => (buttonRefs.current[i] = el)}
                             >
-                              <div className="absolute -top-3 left-1/2 transform -translate-x-1/2">
-                                <Upboxuparrow className="w-4 h-4 text-blue-400" />
+                              <div className="flex justify-center">
+                                <Action className="w-6 h-6 text-blue-500" />
                               </div>
-                              <div className="flex flex-col">
-                                <ul className="space-y-2">
-                                  <li
-                                    className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
-                                    onClick={() => handleEditClick(item?._id)}
-                                  >
-                                    {item?.vilage ? "Edit" : "Add"}
-                                  </li>
-                                  <li
-                                    className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
-                                    onClick={() => handleDelete(item?._id)}
-                                  >
-                                    Delete
-                                  </li>
-                                  <li
-                                    className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
-                                    onClick={() =>
-                                      showQuestionsss(
-                                        item?.mobile,
-                                        item?.vilage
-                                      )
-                                    }
-                                  >
-                                    Send
-                                  </li>
-                                </ul>
+                            </button>
+                            {display && tooltipId === currentLabel && (
+                              <div
+                                role="tooltip"
+                                className="absolute shadow-lg bg-blue-400 z-10 border rounded p-2"
+                                style={{
+                                  top: "100%",
+                                  left: "50%",
+                                  transform: "translateX(-50%)",
+                                }}
+                                ref={tooltipRef}
+                              >
+                                <div className="absolute -top-3 left-1/2 transform -translate-x-1/2">
+                                  <Upboxuparrow className="w-4 h-4 text-blue-400" />
+                                </div>
+                                <div className="flex flex-col">
+                                  <ul className="space-y-2">
+                                    <li
+                                      className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
+                                      onClick={() => handleEditClick(item?._id)}
+                                    >
+                                      {item?.vilage ? "Edit" : "Add"}
+                                    </li>
+                                    <li
+                                      className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
+                                      onClick={() => handleDelete(item?._id)}
+                                    >
+                                      Delete
+                                    </li>
+                                    <li
+                                      className="cursor-pointer hover:bg-blue-300 p-1 rounded text-black font-bold"
+                                      onClick={() =>
+                                        showQuestionsss(
+                                          item?.mobile,
+                                          item?.vilage
+                                        )
+                                      }
+                                    >
+                                      Send
+                                    </li>
+                                  </ul>
+                                </div>
                               </div>
-                            </div>
-                          )}
-                        </>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                            )}
+                          </>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      )}
-
+        )}
+      </div>
       <Showbusnumber
         showQuestion={showQuestion}
         popbox={popbox}
